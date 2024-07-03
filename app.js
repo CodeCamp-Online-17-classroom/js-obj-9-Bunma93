71895f7cd1d968ae44ba375c1ff0cbec815afbec
Bunma93 <bunma93@gmail.com>
2024-07-04 04:56:28 +0700

    Js-obj9

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // write code here
+let obj = {};
+let key = prompt("key");
+
+while (true) {
+    let value = prompt("value");
+    obj[key] = value;
+    key = prompt("key");
+    if (key == "stop") break;
+}
+
+console.log(obj);
